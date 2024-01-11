28ee4d51d53c41b74929f894c02c216a281f4342
Ozhoh, Oleksandr <oleksandrozhoh@gmail.com>
2024-01-10 20:59:42 -0600

    Data types

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let js = "amazing";
-if (js === "amazing") alert("Alert should be printed");
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log(40 + 8 + 23 - 10);
+// // any data type declared with let keyword
+// let name = "Alex";
+// let $function = 28;
+// let PI = 3.14; // coustant values can be named with uppercase
+
+let notAssignedValue;
+console.log(typeof true);
+console.log(typeof "true");
+console.log(typeof 23);
+console.log(typeof notAssignedValue);
+console.log(notAssignedValue); // both DT and value will be equal to undifined
+
+// typeof error:
+console.log(typeof null); // prints object, which is not correct the typeof null is null (bug never corrected for legasy reasons)
